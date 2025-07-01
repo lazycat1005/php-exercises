af29bb53c2be358a207a496d07db688de031c6a1
Rex Wang <starswallow1005@gmail.com>
2025-07-01 15:50:39 +0800

    重構移除字串空格功能，改善HTML結構與CSS樣式，增強可讀性與維護性

--- exercises/97_remove-spaces/version-php/index.php
+++ exercises/97_remove-spaces/version-php/index.php
@@ -5,30 +5,32 @@ include '../../../header.php';
 ?>
 
 <body>
-    <header>
-        <h1>PHP 練習題：移除字串中的空格</h1>
-        <p>給予一個輸入框，送出後 PHP 會清除字串裡所有的空格，然後過濾後的字串重新顯示再輸入框內</p>
-    </header>
+    <div class="remove-spaces">
+        <header class="remove-spaces__header">
+            <h1>PHP 練習題：移除字串中的空格</h1>
+            <p>給予一個輸入框，送出後 PHP 會清除字串裡所有的空格，然後過濾後的字串重新顯示再輸入框內</p>
+        </header>
 
-    <?php
-    $inputString = '';
-    $filteredString = '';
+        <?php
+        $inputString = '';
+        $filteredString = '';
 
-    if (isset($_GET['inputString'])) {
-        $inputString = $_GET['inputString'];
-        $filteredString = preg_replace('/\s+/', '', $inputString);
-    }
-    ?>
+        if (isset($_GET['inputString'])) {
+            $inputString = $_GET['inputString'];
+            $filteredString = preg_replace('/\s+/', '', $inputString);
+        }
+        ?>
 
-    <main>
-        <form action="" method="get">
-            <label for="inputString">請輸入字串：</label>
-            <input type="text" id="inputString" name="inputString" value="<?php echo htmlspecialchars($filteredString); ?>" required>
-            <button type="submit">送出</button>
-        </form>
-    </main>
+        <main class="remove-spaces__main">
+            <form action="" method="get">
+                <label for="inputString">請輸入字串：</label>
+                <input type="text" id="inputString" name="inputString" value="<?php echo htmlspecialchars($filteredString); ?>" required>
+                <button type="submit">送出</button>
+            </form>
+        </main>
 
-    <a class="fixedBtn" href="../../../index.php">Back</a>
+        <a class="fixedBtn" href="../../../index.php">Back</a>
+    </div>
 
 </body>
 
